d72263f28685868b2f7f0771f012236d3fd4cef5
boedegoat <bhremada.fka@gmail.com>
2022-09-04 17:59:07 +0700

    add declaration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/components'),
+            entry: path.resolve(__dirname, 'src/components/index.ts'),
             name: 'React CreditCard Input',
             fileName: (format) => `creditcard-input-react.${format}.js`,
         },
@@ -18,5 +19,10 @@ export default defineConfig({
             },
         },
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
 })
